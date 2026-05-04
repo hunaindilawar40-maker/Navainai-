05f00625d577fe7073f4a25d73c7382de7919014
hunaindilawar40-maker <hunaindilawar40@gmail.com>
2026-05-04 06:54:23 +0500

    Update speak.js

--- api/speak.js
+++ api/speak.js
@@ -7,7 +7,7 @@ module.exports = async function handler(req, res) {
   if(req.method === 'OPTIONS') return res.status(200).end();
 
   const { text } = req.body;
-  const voiceId = 'auq43ws1oslv0tO4BDa7';
+  const voiceId = 'nf4MCGNSdM0hxM95ZBQR';
 
   try {
     const response = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`, {
