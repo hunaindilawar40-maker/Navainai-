fde953f9dca8ab87155335018a69aa8557a6bc23
hunaindilawar40-maker <hunaindilawar40@gmail.com>
2026-05-05 09:00:54 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,3 +1,6 @@
-module.exports = function handler(req, res) {
-  res.json({ key: process.env.NAVAIN_KEY });
+export default function handler(req, res) {
+  res.json({
+    key: process.env.GROQ_API_KEY || '',
+    elevenKey: process.env.ELEVEN_API_KEY || ''
+  });
 }
